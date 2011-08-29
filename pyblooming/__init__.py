de7f997e5b81794c985b747b488ed32e314248d1
Armon Dadgar <armon.dadgar@gmail.com>
2011-08-28 17:17:50 -0700

    Working on a ScalableBloomFilter

--- pyblooming/__init__.py
+++ pyblooming/__init__.py
@@ -1,5 +1,5 @@
 "Exports the important classes"
-__all__ = ["Bitmap", "BloomFilter"]
+__all__ = ["Bitmap", "BloomFilter", "ScalingBloomFilter"]
 __version__ = "0.1.0"
 from bitmap import Bitmap
-from bloom import BloomFilter
+from bloom import BloomFilter, ScalingBloomFilter
--- pyblooming/bloom.py
+++ pyblooming/bloom.py
@@ -28,7 +28,9 @@ class BloomFilter(object):
                 provided.
 
             - length (optional) : The size of the anonymous bitmap
-                in bytes to create. Defaults to 16M.
+                in bytes to create. Defaults to 16M. Must be at least
+                large enough to store the count, which means at least
+                SIZE_LEN bytes (8).
 
             - k (optional) : The number of hashing algorithms to
                 use. Must be at least 1.
@@ -173,4 +175,105 @@ class BloomFilter(object):
         # Unpack
         unpacked = struct.unpack(self.SIZE_FMT, count_str)
         return unpacked[0]
+
+    def __del__(self):
+        "Safety first! Flush the buffers."
+        self.flush()
+
+class ScalingBloomFilter(object):
+    def __init__(self, filters=None, filenames=None, length=16777216, prob=1E-6, k=4, scale_size=4, prob_reduction=0.9):
+        """
+        Creates a new ScalingBloomFilter that tries to enforce
+        a given false positive probability by creating new Bloom Filters
+        and stacking them.
+
+        :Parameters:
+            - filters (optional) : A list of filters to initialize with
+            - filenames (optional) : A callable that generates file names
+              for generating new persisteed BloomFilters. Without this,
+              anonymous bitmaps will be used.
+            - length (optional) : Length of initial filter.
+            - prob (optional) : The false positive rate to enforce.
+            - k (optional) : The number of hash functions to start with.
+            - scale_size (optional) : The file size growth rate. Defaults to 4.
+            - prob_reduction (optional) : The probability reduction with
+              eahc new filter. Defaults to 0.9.
+        """
+        if not callable(filenames): raise ValueError, "Filenames must be callable!"
+        self.filenames = filenames
+        self.prob = prob
+        self.init_length = length
+        self.init_k = k
+        self.scale_size = scale_size
+        self.prob_reduction = prob_reduction
+        self.filters = filters if filters else []
+        self._initialize()
+        if len(self.filters) == 0: self.filters.append(self._create_filter())
+
+    def _initialize(self):
+        "Initializes the probability and capacity of existing filters"
+        prob = self.prob
+        for filt in self.filters:
+            size = len(filt.bitmap)
+            filt.prob = prob
+            filt.capacity = BloomFilter.expected_capacity(size, prob)
+            prob *= self.prob_reduction
+
+    def _create_filter(self):
+        "Creates a new filter"
+        # Get the size of the filter and k val
+        length = self.init_length
+        new_k = self.init_k
+        prob = self.prob
+        if len(self.filters) > 0:
+            length = len(self.filters[-1].bitmap)/8 * self.scale_size
+            new_k = self.filters[-1].k_num + 1
+            prob = self.filters[-1].prob * self.prob_reduction
+
+        # Get the filename
+        filename = None
+        if self.filenames:
+            filename = self.filenames()
+
+        # Create a new bitmap
+        print length, filename
+        bitmap = bitmaplib.Bitmap(length, filename)
+        filter = BloomFilter(bitmap, k=new_k)
+
+        # Add the new properties
+        filter.prob = prob
+        filter.capacity = BloomFilter.expected_capacity(len(bitmap),prob)
+        return filter
+
+    def add(self, key, check_first=True):
+        "Add a key to the set"
+        if not isinstance(key, str): raise ValueError, "Key must be a string!"
+        if check_first and key in self: return False
+
+        # Check if we are over capacity, create a new filter
+        filt = self.filters[-1]
+        if len(filt)+1 >= filt.capacity:
+            filt = self._create_filter()
+            self.filters.append(filt)
+
+        # Add the key to the largest filter
+        return filt.add(key)
+
+    def __contains__(self, key):
+        "Checks if the set contains a given key"
+        if not isinstance(key, str): raise ValueError, "Key must be a string!"
+
+        # Walk over the indexes in reverse order
+        for filt in self.filters[::-1]:
+            if key in filt: return True
+        return False
+
+    def __len__(self):
+        "Returns the number of elements in the bitmap"
+        return sum(len(filt) for filt in self.filters)
+
+    def flush(self):
+        "Flushes all the underlying Bloom filters"
+        for filt in self.filters:
+            filt.flush()
 
