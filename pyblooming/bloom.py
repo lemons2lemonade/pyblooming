d4f4bf25c825368ef60685937e43ba32bf52dee4
Armon Dadgar <armon.dadgar@gmail.com>
2011-11-14 16:03:32 -0800

    Remove size_only flush support

--- pyblooming/bloom.py
+++ pyblooming/bloom.py
@@ -189,7 +189,7 @@ class BloomFilter(object):
         "Returns the number of elements in the bitmap"
         return self.count
 
-    def flush(self, size_only=False):
+    def flush(self):
         """
         Forces us to write out the current count to the bitmap,
         and flushes the underlying bitmap.
@@ -202,7 +202,7 @@ class BloomFilter(object):
         if self.bitmap: self.bitmap[size_offset:size_offset+self.SIZE_LEN] = count_str
 
         # Flush the underlying bitmap
-        if not size_only and self.bitmap: self.bitmap.flush()
+        if self.bitmap: self.bitmap.flush()
 
     def close(self):
         "Closes the bloom filter and the underlying bitmap"
