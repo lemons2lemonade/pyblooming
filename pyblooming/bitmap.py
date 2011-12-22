8fdd495c6f3a19d423609c1516b6615770634cde
Armon Dadgar <armon.dadgar@gmail.com>
2011-12-22 11:54:23 -0800

    Add support for MAP_PRIVATE with file backing. Close take argument to disable flushes.

--- pyblooming/bitmap.py
+++ pyblooming/bitmap.py
@@ -6,7 +6,7 @@ import mmap
 import os.path
 
 class Bitmap(object):
-    def __init__(self, length, filename=None):
+    def __init__(self, length, filename=None, private=False):
         """
         Creates a new Bitmap object. Bitmap wraps a memory mapped
         file and allows bit-level operations to be performed. A bitmap can
@@ -17,14 +17,24 @@ class Bitmap(object):
             is 8 times this.
           - `filename` (optional) : Defaults to None. If this is provided,
             the Bitmap will be file backed.
+          - `private` (optional) : Defaults to False. If True, the bitmap
+            is mapped using MAP_PRIVATE, making a private copy-on-write
+            version of the memory mapped region. Otherwise, MAP_SHARED is used,
+            and changes are reflected to other copies of the file.
         """
         # Save if  the size
         self.size = length
 
-        # Create the fileobj and mmap
+        # Get the mode
+        flags = mmap.MAP_SHARED
+        if private:
+            flags = mmap.MAP_PRIVATE
+
         if not filename:
+            # For an anonymous mmap, we always use private with no file descriptor
             self.fileobj = None
-            self.mmap = mmap.mmap(-1, length)
+            self.mmap = mmap.mmap(-1, length, flags=mmap.MAP_PRIVATE)
+
         else:
             self.fileobj = open(filename, "a+")
 
@@ -35,8 +45,8 @@ class Bitmap(object):
                 self.fileobj.flush()
                 size_diff = length - os.path.getsize(filename)
 
-            # Create the memory mapped file
-            self.mmap = mmap.mmap(self.fileobj.fileno(), length)
+            # Create the memory mapped file, using the proper flags
+            self.mmap = mmap.mmap(self.fileobj.fileno(), length, flags=flags)
 
     def __len__(self):
         "Returns the size of the Bitmap in bits"
@@ -69,10 +79,10 @@ class Bitmap(object):
         if self.mmap: self.mmap.flush()
         if self.fileobj: self.fileobj.flush()
 
-    def close(self):
-        "Closes the Bitmap, first flushing the data."
+    def close(self, flush=True):
+        "Closes the Bitmap, flushing the data if specified."
         # Safety first!
-        self.flush()
+        if flush: self.flush()
 
         # Close the mmap
         if self.mmap: self.mmap.close()
